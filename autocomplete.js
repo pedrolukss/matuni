61f1f6f18418476fb0d896a69deb4b635a74099e
pedrolukss <pedroluka4757@usp.br>
2023-10-19 14:23:57 -0300

    Initial commit

--- autocomplete.js
+++ autocomplete.js
@@ -21,3 +21,13 @@ function display(result){
     });
     resultsBox.innerHTML = '<ul>'+ content.join('') +'</ul>';
 }
+
+/* Set the width of the side navigation to 250px */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
