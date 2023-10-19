972e7ef22852baa5441a4ec16e5f76fcc4885617
pedrolukss <pedroluka4757@usp.br>
2023-10-19 14:23:57 -0300

    Initial commit

---
+++ autocomplete.js
@@ -0,0 +1,23 @@
+let suggestions = ['Um curso de calculo, volume 1, guidorizzi'];
+const resultsBox = document.querySelector(".result-box");
+const inputbox = document.getElementById("input-box");
+inputbox.onkeyup = function (){
+    let result = [];
+    let input = inputbox.value;
+    if (input.length){
+        result = suggestions.filter((keyword)=>{
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+    display(result);
+    if(!result.length){
+        resultsBox.innerHTML = '';
+    }
+}
+function display(result){
+    const content = result.map((list)=>{
+        return '<li>' + list + '</li>';
+    });
+    resultsBox.innerHTML = '<ul>'+ content.join('') +'</ul>';
+}
